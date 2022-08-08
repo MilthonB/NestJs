0e60a84e998ccc7cf9855d630fed59f29aa63ad3
Milthon <milthonBor@gmail.com>
2022-08-07 22:35:51 -0700

    obtencio por id

--- 01-car-dealership/src/cars/cars.controller.ts
+++ 01-car-dealership/src/cars/cars.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 
 @Controller('cars')
 export class CarsController {
 
+    private cars = ['Toyota','Honda','Jeep']
+
     @Get()
     getAllCars(){
-        return ['Toyota','Honda','Jeep']
+        return this.cars
     }
+
+    @Get(":id")
+    getCarById( @Param('id') id:number){
+        console.log(id);
+        
+        return {
+            id,
+            car: this.cars[1]
+        }
+    }
+
+
 
 }
