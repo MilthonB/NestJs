a027ca7208121415cfe8a4f41cc9cb916d2105cc
Milthon <milthonBor@gmail.com>
2022-08-09 20:28:10 -0700

    class validator y class-tranform pipe globales

--- 01-car-dealership/src/main.ts
+++ 01-car-dealership/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // solo deja la data que se espera
+      forbidNonWhitelisted: true, // Lanza un 400 con los compos que vienen pero que no estan en la whitelist
+    })
+  );
+
   await app.listen(4200);
+
+
+
+
 }
 bootstrap();
