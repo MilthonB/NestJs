7becf826fa3571fb8a426e12df64664cc7dbadd7
Milthon <milthonBor@gmail.com>
2022-08-15 21:45:08 -0700

    config docker mongo

--- 02-pokedex/src/main.ts
+++ 02-pokedex/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api/v2') // prefijo global para el endpoint
 
 
   await app.listen(3000);
