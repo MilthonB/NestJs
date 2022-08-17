237a07ad2bbe3821aa820de1ff51b86b9b8703b9
Milthon <milthonBor@gmail.com>
2022-08-16 17:45:02 -0700

    CReacion de modelos y esquemas

--- 02-pokedex/src/pokemon/entities/pokemon.entity.ts
+++ 02-pokedex/src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,29 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+// la clase se tiene que extender de Document para obtener 
+@Schema() // Asignar que es un schema de una base de datos
+export class Pokemon extends Document {
+
+    // id: string // Mongo lo asigna
+
+    // Asignar propiedades a los valores de la tabla 
+    @Prop({
+        unique: true,
+        index: true
+    })
+    name: string;
+
+    @Prop({
+        unique: true,
+        index: true
+    })
+    no: string;
+
+}
+
+// Se exporta un schema con elo documento creado
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
+
+
+
