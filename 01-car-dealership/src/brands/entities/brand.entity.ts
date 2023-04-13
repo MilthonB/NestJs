9d24eeda73c33f7edb0bfbbfa50c997b62e9d738
MilthonB <milthonbor@gmail.com>
2023-04-12 19:05:30 -0700

    CRUD

--- 01-car-dealership/src/brands/entities/brand.entity.ts
+++ 01-car-dealership/src/brands/entities/brand.entity.ts
@@ -1,1 +1,15 @@
-export class Brand {}
+export class Brand {
+
+
+    // asi luciria la informacion en la tabla
+    id:   string;
+    name: string;
+
+
+
+    createdAt: number;
+    updatedAt?: number;
+
+
+
+}
